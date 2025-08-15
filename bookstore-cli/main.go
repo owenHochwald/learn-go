7542f434ec9ad57486347e770b1eec45943016e1
owenhochwald <owenhochwald@gmail.com>
2025-08-15 15:46:30 -0400

    add load books function

--- bookstore-cli/main.go
+++ bookstore-cli/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 )
 
+type Book struct {
+	Id        string `json:"id"`
+	Title     string `json:"title"`
+	Author    string `json:"author"`
+	Image_url string `json:"imageUrl"`
+}
 
-func main () {
+const filePath = "books.json"
+
+func main() {
 
 	// check file for errors
-	
+	books := loadBooks()
+
+	fmt.Print(books)
 
 	args := os.Args[1:]
 
@@ -22,5 +33,31 @@ func main () {
 	}
 
 	fmt.Println(args)
+
+}
+
+func loadBooks() []Book {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		// create the file
+		err := os.WriteFile(filePath, []byte("[]"), 0644)
+		if err != nil {
+			panic(err)
+		}
+		return []Book{}
+	}
+
+	// open file and read contents to memory in data
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	// load books from file into books var
+	var books []Book
+	if err := json.Unmarshal(data, &books); err != nil {
+		panic(err)
+	}
+
+	return books
 
 }
