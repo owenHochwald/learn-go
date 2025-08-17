8dfc2a2709f6bbfc87b9c9be5719f73a8830f3f7
owenhochwald <owenhochwald@gmail.com>
2025-08-17 09:51:46 -0400

    abstract file loading and writing into utils

--- bookstore-cli/main.go
+++ bookstore-cli/main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"bookstore-cli/models"
 	"bookstore-cli/services"
-	"encoding/json"
+	"bookstore-cli/utils"
 	"fmt"
 	"os"
 )
 
-const filePath = "books.json"
-
 func main() {
 
 	// check file for errors
-	books := loadBooks()
+	books := utils.LoadBooks()
 
 	fmt.Println(books)
 
@@ -33,44 +30,6 @@ func main() {
 
 	fmt.Println(args)
 
-}
+	utils.WriteBooks(books)
 
-func loadBooks() []models.Book {
-	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		// create the file
-		err := os.WriteFile(filePath, []byte("[]"), 0644)
-		if err != nil {
-			panic(err)
-		}
-		return []models.Book{}
-	}
-
-	// open file and read contents to memory in data
-	data, err := os.ReadFile(filePath)
-	if err != nil {
-		panic(err)
-	}
-
-	// load books from file into books var
-	var books []models.Book
-	if err := json.Unmarshal(data, &books); err != nil {
-		panic(err)
-	}
-
-	return books
-}
-
-func writeBooks(books []models.Book) {
-	// get data and marshal into json
-	data, err := json.MarshalIndent(books, "", "  ")
-	if err != nil {
-		panic("Error marshalling books" + err.Error())
-	}
-	// write into books.json
-	err = os.WriteFile(filePath, data, 0666)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(("Saved books to books.json"))
 }
---
+++ bookstore-cli/utils/files.go
@@ -0,0 +1,52 @@
+package utils
+
+import (
+	"bookstore-cli/models"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+const filePath = "books.json"
+
+func LoadBooks() []models.Book {
+	createFile()
+
+	// open file and read contents to memory in data
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	// load books from file into books var
+	var books []models.Book
+	if err := json.Unmarshal(data, &books); err != nil {
+		panic(err)
+	}
+
+	return books
+}
+
+func createFile() {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		err := os.WriteFile(filePath, []byte("[]"), 0644)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func WriteBooks(books []models.Book) {
+	// get data and marshal into json
+	data, err := json.MarshalIndent(books, "", "  ")
+	if err != nil {
+		panic("Error marshalling books" + err.Error())
+	}
+	// write into books.json
+	err = os.WriteFile(filePath, data, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(("Saved books to books.json"))
+}
