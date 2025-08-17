94c544c4c2df49f1cf27f87bedb0f54cd9f6163c
owenhochwald <owenhochwald@gmail.com>
2025-08-17 09:35:51 -0400

    refactor book CRUD into book_service

--- bookstore-cli/main.go
+++ bookstore-cli/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"bookstore-cli/models"
+	"bookstore-cli/services"
 	"encoding/json"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 const filePath = "books.json"
@@ -17,9 +17,9 @@ func main() {
 
 	fmt.Println(books)
 
-	getAllBooks(books)
+	services.GetAllBooks(books)
 
-	getBook(1, books)
+	services.GetBook(1, books)
 
 	args := os.Args[1:]
 
@@ -58,71 +58,4 @@ func loadBooks() []models.Book {
 	}
 
 	return books
-}
-
-func getAllBooks(books []models.Book) {
-	printHeader()
-
-	// print all books to console
-	for _, book := range books {
-		printBook(book)
-	}
-	fmt.Println()
-
-}
-
-func printHeader() {
-	fmt.Println("Id\t\tTitle\t\tAuthor\t\tPrice\t\tImage URL")
-}
-
-func getBook(id int, books []models.Book) *models.Book {
-	for _, book := range books {
-		bookId, err := strconv.Atoi(book.Id)
-		if err != nil {
-			panic(err)
-		}
-
-		if bookId == id {
-			printHeader()
-			printBook(book)
-			fmt.Println()
-		}
-		return &book
-	}
-	panic("Book not found")
-}
-
-func addBook(title, author, price, imageUrl string, books []models.Book) []models.Book {
-	book := models.Book{
-		Id:        strconv.Itoa(len(books)),
-		Title:     title,
-		Author:    author,
-		Price:     price,
-		Image_url: imageUrl}
-
-	return append(books, book)
-}
-
-func updateBook(id, title, author, price, imageUrl string, books []models.Book) []models.Book {
-	bookId, _ := strconv.Atoi(id)
-	book := getBook(bookId, books)
-
-	book.Title = title
-	book.Author = author
-	book.Price = price
-	book.Image_url = imageUrl
-	return books
-
-}
-
-func deleteBook(id string, books []models.Book) []models.Book {
-	for i, book := range books {
-		if book.Id == id {
-			return append(books[:i], books[i+1:]...)
-		}
-	}
-	return []models.Book{}
-}
-func printBook(book models.Book) {
-	fmt.Printf("%s\t%s\t%s\t%s\t%s\n", book.Id, book.Title, book.Author, book.Price, book.Image_url)
 }
