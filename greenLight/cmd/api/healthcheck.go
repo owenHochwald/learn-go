27a11b771a3c11629fb0f4ca9f061a13995efb47
owenhochwald <owenhochwald@gmail.com>
2025-08-24 22:20:22 -0400

    final commit before moving greenlight

--- greenLight/cmd/api/healthcheck.go
+++ greenLight/cmd/api/healthcheck.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 )
 
 func (app *application) healthCheckHandler(c *gin.Context) {
-	fmt.Fprint(c.Writer, "version %s", version)
-	fmt.Fprint(c.Writer, "port %d", app.config.port)
-	c.Writer.WriteHeader(200)
+	c.JSON(200, gin.H{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	})
 }
